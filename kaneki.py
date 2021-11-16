c2e8aa773f07d65b006cb12a9da65f7fc45714b8
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-11-16 14:16:15 +0700

    Update kaneki.py

--- kaneki.py
+++ kaneki.py
@@ -20,9 +20,9 @@ bot = Client('Feedback bot',
 
 owner_id=C.OWNER_ID
 
-#LOG_TEXT = "╟᪣ ID Kamu : <code>{}</code>\n╟᪣ Nama : <a href='tg://user?id={}'></a>\n╟᪣ Username : https://t.me/{message.chat.username}<>╟᪣ DC ID: <code>{}</code>"
+#LOG_TEXT = "╟᪣ ID Kamu : <code>{}</code>\n╟᪣ Nama : <a href='tg://user?id={}'>{}{}</a>\n╟᪣ Username : href=https://t.me/{message.chat.username}>{}{}</a>╟᪣ DC ID: <code>{}</code>"
 
-IF_TEXT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
+IF_TEXT = "<b>Message from:</b> {}\n<b>Name:</b> {}"
 
 IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
 
@@ -30,12 +30,12 @@ IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
 async def start(bot, message):
     await bot.send_message(
         chat_id=owner_id,
-        text = "<b>╟᪣ ID Kamu : <code>{}</code>\n╟᪣ Nama : <a href='tg://user?id={}'></a>\n╟᪣ Username : https://t.me/{message.chat.username}<>╟᪣ DC ID: <code>{}</code></b>",
+        text = "<b>╟᪣ ID Kamu : <code>{}</code>\n╟᪣ Nama : <a href='tg://user?id={}'>{}</a>\n╟᪣ Username : url="https://t.me/{client.username}?start={message.command[1]}">{}</a>╟᪣ DC ID: <code>{}</code></b>",
         #text=LOG_TEXT.format(message.chat.id,message.chat.id,message.chat.first_name,message.chat.username,message.chat.last_name,message.chat.dc_id),
         parse_mode="html"
     )
     await message.reply_text(
-        text="**Hi {}!**\n".format(message.chat.first_name),
+        text="**Hi {}!**\n".format(message.chat.first_name)+C.START,
         reply_markup=InlineKeyboardMarkup([
             [ InlineKeyboardButton(text="GROUP : <a href='https://t.me/ToxicZoneGc'>ᴅɪsɪɴɪ</a>")]
         ])
