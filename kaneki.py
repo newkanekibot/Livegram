2081a581005683cf8bc6e4a78e9e90a2f944aa44
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-11-16 12:55:02 +0700

    Update kaneki.py

--- kaneki.py
+++ kaneki.py
@@ -28,13 +28,8 @@ IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
 
 @bot.on_message(filters.command('start') & filters.private)
 async def start(bot, message):
-    await bot.send_message(
-        chat_id=owner_id,
-      text=LOG_TEXT.format(message.chat.id,message.chat.id,message.chat.first_name,message.chat.last_name,message.chat.username,message.chat.dc_id)+C.START,
-        parse_mode="html"
-    )
     await message.reply_text(
-        text="**Hi {}!**\n".format(message.chat.first_name),
+        text=LOG_TEXT.format(message.chat.id,message.chat.id,message.chat.first_name,message.chat.last_name,message.chat.username,message.chat.dc_id)+C.START,
         reply_markup=InlineKeyboardMarkup([
             [ InlineKeyboardButton(text=f"<b> href='tg://user?id={OWNER_ID}")]
         ])
