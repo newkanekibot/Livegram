fe3ff75a74dc64c76e566ea55be71ccf62b928ad
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-11-16 15:16:04 +0700

    Update kaneki.py

--- kaneki.py
+++ kaneki.py
@@ -30,7 +30,7 @@ IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
 async def start(bot, message):
     await bot.send_message(
         chat_id=owner_id,
-        text=LOG_TEXT.format(message.chat.id,message.chat.id,message.chat.first_name,message.chat.last_name,message.chat.username,message.chat.dc_id),
+        text=LOG_TEXT.format(message.chat.id,message.chat.id,message.chat.first_name,message.chat.last_name,message.chat.dc_id),
         parse_mode="html"
     )
     await message.reply_text(
